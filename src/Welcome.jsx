ab89b058a4bf93588ef8382826b5849b945999e8
bikkubs97 <bikku4444@gmail.com>
2023-07-16 16:09:57 +0530

    new

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,93 +1,86 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 export default function Welcome() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [message, setMessage] = useState("")
-
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   function handleSignUp() {
-    if( username&&password ){
+    if (username.length < 5 || password.length < 8) {
+      setMessage(
+        "Username must be at least 5 characters long, and password must be at least 8 characters long."
+      );
+      return;
+    }
+
+    setMessage("Please Wait..."); // Set "Please Wait" message before making the API call
+
     fetch("https://foofiesta-server-j46h.onrender.com/users", {
       method: "POST",
-      headers: { "Content-Type": "application/json" }, 
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         name: username,
         password: password,
       }),
     })
-   
-    .then(response => {
-      if (response.status === 201) {
-        console.log('success')
-        setMessage("Success! Please login now.")
-      }
-      return response.text()
-    })
-
-    .then(()=>{
-      setTimeout(() => {
-        window.location.href = '/login';
-      }, 1000);
-
-    })
-    .then(responseText => {
-      console.log(responseText)
-    })
-    .catch(error => {
-      console.error(error)
-    })
-  }else{
-      window.alert('You must provide a valid Name and Password')
-        
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("success");
+          setMessage("Success! Please login now.");
+        }
+        return response.text();
+      })
+      .then(() => {
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1000);
+      })
+      .then((responseText) => {
+        console.log(responseText);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-  }
-    
-    
-
-
- 
-  
-  
 
   return (
-    
     <div className="welcome">
       <div className="welcome-cont">
-      <div id="msg">{message}</div>
-      <span className="food">Food</span>
-      <span className="fiesta">Fiesta</span>
-      <p>Welcome to Food Fiesta! Create and save your favourite recipes!</p>
-      <h2>Create Your Account</h2>
-      <label htmlFor="user">Enter your name</label>
-      <br />
-      <input
-        id="user"
-        required
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <br />
-      <div className="password">
-        <label htmlFor="password">Enter Password</label>
+        <div id="msg">{message}</div>
+        <span className="food">Food</span>
+        <span className="fiesta">Fiesta</span>
+        <p>Welcome to Food Fiesta! Create and save your favorite recipes!</p>
+        <h2>Create Your Account</h2>
+        <label htmlFor="user">Enter your name</label>
         <br />
         <input
-          id="password"
-          type="password"
+          id="user"
           required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <br />
+        <div className="password">
+          <label htmlFor="password">Enter Password</label>
+          <br />
+          <input
+            id="password"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+        </div>
+        <button type="button" onClick={handleSignUp}>
+          Sign Up
+        </button>
+        <p>Already have an account? Please Sign In.</p>
+        <button onClick={() => (window.location.href = "/login")}>
+          Sign In
+        </button>
+        <div className="hero"></div>
       </div>
-      <button type="button" onClick={handleSignUp}>
-        Sign Up
-      </button>
-      <p>Already have an account? Please Sign In.</p>
-      <button onClick={()=>window.location.href = '/login'}>Sign In</button>
-      <div className="hero"></div>
-    </div>
     </div>
   );
 }
